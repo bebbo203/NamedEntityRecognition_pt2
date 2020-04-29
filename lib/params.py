f19c6efc17c3842f4053cbb4d5e56707bccf369b
Roberto Aureli <bebbo203@gmail.com>
2020-04-29 18:49:49 +0200

    purh

--- lib/params.py
+++ lib/params.py
@@ -2,19 +2,19 @@ class Params():
     device = "cuda"
     test = False
     min_freq = 2
-    windows_size = 100
-    windows_shift = 100
+    windows_size = 175
+    windows_shift = 175
     word_embedding_size = 200
     char_word_embedding_size = 50
     hidden_dim = 256
-    char_hidden_dim = 128
+    char_hidden_dim = 256
     bidirectional = True
     num_layers = 2
-    dropout = 0.4
-    max_word_length= 15
+    dropout = 0.6
+    max_word_length= 10
     embeddings_path = None
     embeddings_path = "model/glove.6B.200d.txt"
     processed_embeddings_path = "model/processed_embeddings_path.json"
     alphabet_size = 67
-    char_embedding_size = 8
+    char_embedding_size = 10
     #
