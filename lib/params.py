31b66656f2c1fbb60b81afc7ff54e2e2dd000f0f
Roberto Aureli <bebbo203@gmail.com>
2020-04-29 19:29:20 +0200

    pu

--- lib/params.py
+++ lib/params.py
@@ -10,11 +10,11 @@ class Params():
     char_hidden_dim = 256
     bidirectional = True
     num_layers = 2
-    dropout = 0.6
-    max_word_length= 10
+    dropout = 0.5
+    max_word_length= 15
     embeddings_path = None
     embeddings_path = "model/glove.6B.200d.txt"
     processed_embeddings_path = "model/processed_embeddings_path.json"
     alphabet_size = 67
-    char_embedding_size = 10
+    char_embedding_size = 15
     #
