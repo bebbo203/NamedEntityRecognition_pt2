10e4097088f63061864ea145d8fecd743fd6b684
Roberto Aureli <bebbo203@gmail.com>
2020-04-30 09:13:13 +0200

    lungo

--- lib/params.py
+++ lib/params.py
@@ -5,16 +5,16 @@ class Params():
     windows_size = 175
     windows_shift = 175
     word_embedding_size = 200
-    char_word_embedding_size = 50
+    char_word_embedding_size = 100
     hidden_dim = 256
     char_hidden_dim = 256
     bidirectional = True
     num_layers = 2
     dropout = 0.5
-    max_word_length= 15
+    max_word_length= 10
     embeddings_path = None
     embeddings_path = "model/glove.6B.200d.txt"
     processed_embeddings_path = "model/processed_embeddings_path.json"
     alphabet_size = 67
-    char_embedding_size = 15
+    char_embedding_size = 25
     #
